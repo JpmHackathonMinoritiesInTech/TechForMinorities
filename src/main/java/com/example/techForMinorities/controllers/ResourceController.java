3b846d23612c63cbb1d4701bf5c7260fd614f050
adamyoungpeter <adamyoungpeter@gmail.com>
2021-09-16 11:20:44 -0400

    added controller

---
+++ src/main/java/com/example/techForMinorities/controllers/ResourceController.java
@@ -0,0 +1,36 @@
+package com.example.techForMinorities.controllers;
+
+import com.example.techForMinorities.repositories.BootcampInfoRepo;
+import com.example.techForMinorities.repositories.InterviewHelpRepo;
+import com.example.techForMinorities.repositories.MentorsRepo;
+import com.example.techForMinorities.repositories.SelfDirectedRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ResourceController {
+
+    private BootcampInfoRepo bootcampInfoRepo;
+    private InterviewHelpRepo interviewHelpRepo;
+    private MentorsRepo mentorsRepo;
+    private SelfDirectedRepo selfDirectedRepo;
+
+    public ResourceController(BootcampInfoRepo bootcampInfoRepo, InterviewHelpRepo interviewHelpRepo,
+                              MentorsRepo mentorsRepo, SelfDirectedRepo selfDirectedRepo){
+        this.bootcampInfoRepo = bootcampInfoRepo;
+        this.interviewHelpRepo = interviewHelpRepo;
+        this.mentorsRepo = mentorsRepo;
+        this.selfDirectedRepo = selfDirectedRepo;
+    }
+
+    @GetMapping("/resources")
+    public String displayResourcesPage(Model model){
+        model.addAttribute("bootCampResources", bootcampInfoRepo.findAll());
+        model.addAttribute("interviewHelpResources", interviewHelpRepo.findAll());
+        model.addAttribute("mentorResources", mentorsRepo.findAll());
+        model.addAttribute("selfDirectedResources", selfDirectedRepo.findAll());
+
+        return "resources.html";
+    }
+}
