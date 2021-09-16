54cd9099206f06c7ae0f87b6cb519af223e4a9cb
adamyoungpeter <adamyoungpeter@gmail.com>
2021-09-16 10:40:08 -0400

    adding gradle/spring

---
+++ src/main/java/com/example/techForMinorities/TechForMinoritiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.techForMinorities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechForMinoritiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechForMinoritiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/techForMinorities/TechForMinoritiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techForMinorities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechForMinoritiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
